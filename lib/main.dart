2b27d5a9851fd251276e0d4b07c16f60239a8e44
Ankit Lakra <ankit.lakra@datapeace.in>
2024-08-31 20:17:23 +0530

    feat: added firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -12,6 +14,12 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase using the DefaultFirebaseOptions object exported by the configuration file
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
